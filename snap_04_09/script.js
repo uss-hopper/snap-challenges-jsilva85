7db557e63adfc3fa16f2061cb61c00a75413de6d
Jesse Silva <thebestjesse76@gmail.com>
2019-04-09 14:26:42 -0600

    SNAP CHALLENGES

---
+++ snap_04_09/script.js
@@ -0,0 +1,63 @@
+/**window.onload = function () {
+    snap-challenges-jsilva85 ();
+}
+
+function fSum($5, 10) {
+    *return firstNumber + secondNumber;
+}
+
+*function getRandomInt(min, max) {
+    *min = Math.ceil(min);
+    *max = Math.floor(max);
+    *return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+ /*
+
+/**
+ * Take two numbers and multiply them.
+ *
+ * @param int firstNumber One number to multiply.
+ * @param int secondNumber The other number to multiply.
+ *@return int The product of the two nubmers.
+ */
+function multiplyNumbers (firstNumber, secondNumber) {
+    return  firstNumber * secondNumber;
+
+}
+
+let myNumber = -5
+let firstNumber = 29;
+
+
+console.log(firstNumber);
+
+
+console.log(multiplyNumbers(-2, myNumber));
+console.log(multiplyNumbers(2, myNumber));
+console.log(multiplyNumbers(-1, 5));
+console.log(multiplyNumbers(-2, 5));
+
+console.log(firstNumber);
+
+/**
+ * Take two names and concatenate them.
+ *
+ *@param string firstName The given name of concatenate.
+ * @param string lastName The family name to concatenate.
+ * @return string The full name of our person.
+ */
+
+function getFullName (firstName, lastName) {
+    return firstName + '' + lastName
+
+}
+myFirstName = 'Barry';
+myFirstName = 'Susanna';
+
+console.log(getFullName(myFirstName, 'Schulzetenberg'));
+console.log(getFullName("Paul", "Schulzetenberg"));
+
+myFirstName = 'Susanna';
+
+
+console.log(getFullName(myFirstName, 'Schulzetenberg'));
